9aaef43ce2a9770da43dba1ffde6b92746759494
Ady G (Work) <Adrian.Gould@nmtafe.wa.edu.au>
2025-10-29 16:44:10 +0800

    wip(categories): Category Management (staff+)
    - Admin/CategoryManagementController
      - index
      - edit
      - show

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,18 +15,26 @@ class CategorySeeder extends Seeder
     {
         $seedCategories = [
             ['id' => 1, 'title' => 'unknown',],
+            ['description' => 'This will not be added'],
             ['id' => 100, 'title' => 'pirate', 'description' => "Aaaaarrrrgh! Me Jocular hearties!",],
-            ['title'=>'lightbulb', 'description'=>'Some bright spark thinks they are funny.',],
-            ['title'=>'dad','description'=>"Don't you love the bad jokes dad's tell.",],
-            ['title'=>'pun','description'=>'These are usually very corny.',],
-            ['title'=>'knock-knock','description'=>'You know you need to check the door...',],
+            ['title' => 'lightbulb', 'description' => 'Some bright spark thinks they are funny.',],
+            ['title' => 'dad', 'description' => "Don't you love the bad jokes dad's tell.",],
+            ['title' => 'pun', 'description' => 'These are usually very corny.',],
+            ['title' => 'knock-knock', 'description' => 'You know you need to check the door...',],
         ];
 
-        foreach ($seedCategories as $seedCategory){
-            $category = Category::firstOrCreate(
-                ['title'=>$seedCategory['title'] ?? 0],
-                $seedCategory
-            );
+        foreach ($seedCategories as $seedCategory) {
+
+            // only insert the category if the title is not empty
+            $title = $seedCategory['title'] ?? null;
+            if (!is_null($title)) {
+
+                $category = Category::firstOrCreate(
+                    ['title' => $title],
+                    $seedCategory
+                );
+
+            }
         }
     }
 }
